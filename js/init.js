bd1c5a063ff520dcab160008b0207f10e01a0684
Xyinkl <Xyinkl@Gen8-CentOS.lan>
2024-07-17 02:11:39 +0800

    bug found 3

--- js/init.js
+++ js/init.js
@@ -299,24 +299,37 @@ function edrea_tm_news_popup(){
 // -----------------------------------------------------
 // ---------------   PRELOADER   -----------------------
 // -----------------------------------------------------
-function isMobileDevice() {
-    return /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent);
+
+function edrea_tm_preloader(){
+	
+	"use strict";
+	
+	var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ? true : false;
+	var preloader = $('#preloader');
+	
+	if (!isMobile) {
+		setTimeout(function() {
+			preloader.addClass('preloaded');
+		}, 800);
+		setTimeout(function() {
+			preloader.remove();
+		}, 2000);
+
+	} else {
+		preloader.remove();
+	}
 }
 
-function edrea_tm_preloader() {
-    "use strict";
-    
-    var preloader = $('#preloader');
-    
-    preloader.remove();
-        
-}
+// -----------------------------------------------------
+// -----------------   MY LOAD    ----------------------
+// -----------------------------------------------------
 
-function edrea_tm_my_load() {
-    "use strict";
-    
-    var speed = 500;
-    setTimeout(edrea_tm_preloader, speed);
+function edrea_tm_my_load(){
+	
+	"use strict";
+	
+	var speed	= 500;
+	setTimeout(function(){edrea_tm_preloader();},speed);
 }
 
 // -----------------------------------------------------
